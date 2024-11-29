854ec6165503b9603d52b39678f6ed13ab37c9a7
poach3r <n0nam3b01@gmail.com>
2024-11-29 02:57:23 +0000

    updated gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,40 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("com.github.johnrengelman.shadow") version "8.0.0"
+    id("org.graalvm.buildtools.native") version "0.10.3"
 }
 
 group = "org.poach3r"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("pola")
+        archiveVersion.set("0.1.0")
+        manifest {
+            attributes["Main-Class"] = "org.poach3r.MainKt"
+        }
+    }
+
+    graalvmNative {
+        binaries {
+            named("main") {
+                imageName.set("pola")
+                mainClass.set("org.poach3r.MainKt")
+                buildArgs.add("-O4")
+            }
+        }
+    }
 }
